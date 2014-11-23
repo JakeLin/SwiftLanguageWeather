982cfaea1f599289377f8dad7fa8a50d48a2604d
gizmou <marc.tarnutzer@gmail.com>
2014-11-23 20:59:13 +0100

    deleted some unnecessary stuff
    Ready for pull request version of the forecast feature

--- Swift Weather/ViewController.swift
+++ Swift Weather/ViewController.swift
@@ -61,7 +61,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func updateWeatherInfo(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
         let manager = AFHTTPRequestOperationManager()
-        //let url = "http://api.openweathermap.org/data/2.5/weather"
+        
         let url = "http://api.openweathermap.org/data/2.5/forecast"
         println(url)
         
@@ -122,9 +122,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             if let weatherArray = (jsonResult["list"]? as? NSArray) {
                 for index in 0...4 {
                     if let perTime = (weatherArray[index] as? NSDictionary) {
-                        println("1")
                         if let main = (perTime["main"]? as? NSDictionary) {
-                            println("2")
                             var temp = (main["temp"] as Double)
                             if (cntry == "US") {
                                 // Convert temperature to Fahrenheit if user is within the US
@@ -138,7 +136,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                             // FIXED: Is it a bug of Xcode 6? can not set the font size in IB.
                             //self.temperature.font = UIFont.boldSystemFontOfSize(60)
                             if (index==1) {
-                                println("im in ")
                                 self.temp1.text = "\(temperature)°"
                             }
                             if (index==2) {
@@ -150,7 +147,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                             if (index==4) {
                                 self.temp4.text = "\(temperature)°"
                             }
-                            println("sup")
                         }
                         var dateFormatter = NSDateFormatter()
                         dateFormatter.dateFormat = "HH:mm"
@@ -158,7 +154,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                             let thisDate = NSDate(timeIntervalSince1970: date)
                             let forecastTime = dateFormatter.stringFromDate(thisDate)
                             if (index==1) {
-                                println("im in ")
                                 self.time1.text = forecastTime
                             }
                             if (index==2) {
@@ -287,132 +282,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
 
-    
-    func updateUISuccess2(jsonResult: NSDictionary!) {
-        self.loading.text = nil
-        self.loadingIndicator.hidden = true
-        self.loadingIndicator.stopAnimating()
-        
-        if let tempResult = ((jsonResult["main"]? as NSDictionary)["temp"] as? Double) {
-            
-            // If we can get the temperature from JSON correctly, we assume the rest of JSON is correct.
-            var temperature: Double
-            if let sys = (jsonResult["sys"]? as? NSDictionary) {
-                if let country = (sys["country"] as? String) {
-                    if (country == "US") {
-                        // Convert temperature to Fahrenheit if user is within the US
-                        temperature = round(((tempResult - 273.15) * 1.8) + 32)
-                    }
-                    else {
-                        // Otherwise, convert temperature to Celsius
-                        temperature = round(tempResult - 273.15)
-                    }
-                    
-                    // Is it a bug of Xcode 6? can not set the font size in IB.
-                    self.temperature.font = UIFont.boldSystemFontOfSize(60)
-                    self.temperature.text = "\(temperature)°"
-                }
-                
-                if let name = jsonResult["name"] as? String {
-                    self.location.font = UIFont.boldSystemFontOfSize(25)
-                    self.location.text = name
-                }
-                
-                if let weather = jsonResult["weather"]? as? NSArray {
-                    var condition = (weather[0] as NSDictionary)["id"] as Int
-                    var sunrise = sys["sunrise"] as Double
-                    var sunset = sys["sunset"] as Double
-                    
-                    var nightTime = false
-                    var now = NSDate().timeIntervalSince1970
-                    // println(nowAsLong)
-                    
-                    if (now < sunrise || now > sunset) {
-                        nightTime = true
-                    }
-                    //self.updateWeatherIcon(condition, nightTime: nightTime)
-                    return
-                }
-            }
-        }
-        self.loading.text = "Weather info is not available!"
-    }
-    
-    // Converts a Weather Condition into one of our icons.
-    // Refer to: http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
-    func updateWeatherIcon2(condition: Int, nightTime: Bool) {
-        // Thunderstorm
-        if (condition < 300) {
-            if nightTime {
-                self.icon.image = UIImage(named: "tstorm1_night")
-            } else {
-                self.icon.image = UIImage(named: "tstorm1")
-            }
-        }
-        // Drizzle
-        else if (condition < 500) {
-            self.icon.image = UIImage(named: "light_rain")
-        }
-        // Rain / Freezing rain / Shower rain
-        else if (condition < 600) {
-            self.icon.image = UIImage(named: "shower3")
-        }
-        // Snow
-        else if (condition < 700) {
-            self.icon.image = UIImage(named: "snow4")
-        }
-        // Fog / Mist / Haze / etc.
-        else if (condition < 771) {
-            if nightTime {
-                self.icon.image = UIImage(named: "fog_night")
-            } else {
-                self.icon.image = UIImage(named: "fog")
-            }
-        }
-        // Tornado / Squalls
-        else if (condition < 800) {
-            self.icon.image = UIImage(named: "tstorm3")
-        }
-        // Sky is clear
-        else if (condition == 800) {
-            if (nightTime){
-                self.icon.image = UIImage(named: "sunny_night") // sunny night?
-            }
-            else {
-                self.icon.image = UIImage(named: "sunny")
-            }
-        }
-        // few / scattered / broken clouds
-        else if (condition < 804) {
-            if (nightTime){
-                self.icon.image = UIImage(named: "cloudy2_night")
-            }
-            else{
-                self.icon.image = UIImage(named: "cloudy2")
-            }
-        }
-        // overcast clouds
-        else if (condition == 804) {
-            self.icon.image = UIImage(named: "overcast")
-        }
-        // Extreme
-        else if ((condition >= 900 && condition < 903) || (condition > 904 && condition < 1000)) {
-            self.icon.image = UIImage(named: "tstorm3")
-        }
-        // Cold
-        else if (condition == 903) {
-            self.icon.image = UIImage(named: "snow5")
-        }
-        // Hot
-        else if (condition == 904) {
-            self.icon.image = UIImage(named: "sunny")
-        }
-        // Weather condition is not available
-        else {
-            self.icon.image = UIImage(named: "dunno")
-        }
-    }
-    
     /*
     iOS 8 Utility
     */
