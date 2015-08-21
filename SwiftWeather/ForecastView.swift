dfafeccf36e006c8b763c51ae62825942e3ccd86
Jake Lin <jakelinau@gmail.com>
2015-08-22 00:05:03 +1000

    Add custom ForecastView

---
+++ SwiftWeather/ForecastView.swift
@@ -0,0 +1,35 @@
+//
+//  ForecastView.swift
+//  SwiftWeather
+//
+//  Created by Jake Lin on 8/21/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ForecastView: UIView {
+    // Our custom view from the XIB file
+    var view: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib()
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "ForecastView", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
+        view.frame = bounds
+        view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(view);
+        return view
+    }
+}
