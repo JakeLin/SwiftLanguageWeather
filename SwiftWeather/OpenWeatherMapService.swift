b2c5de84bcac34e7d461f22e3a9be15bdf7d7398
Jake Lin <jakelinau@gmail.com>
2015-09-02 17:57:17 +1000

    Render the data using `ForecastViewModel`

--- SwiftWeather/OpenWeatherMapService.swift
+++ SwiftWeather/OpenWeatherMapService.swift
@@ -8,7 +8,13 @@ import CoreLocation
 
 class OpenWeatherMapService : WeatherServiceProtocol {
   func retrieveWeatherInfo(location: CLLocation) -> Weather {
-    let weather: Weather = Weather(location: "Sydney", iconText: "", temperature: "10", forecasts: [])
+    let forecasts = [
+        Forecast(time: "10:00", iconText: "", temperature: "10"),
+        Forecast(time: "12:00", iconText: "", temperature: "8"),
+        Forecast(time: "16:00", iconText: "", temperature: "12"),
+        Forecast(time: "20:00", iconText: "", temperature: "14"),
+    ]
+    let weather = Weather(location: "Sydney", iconText: "", temperature: "10", forecasts: forecasts)
     return weather
   }
 }
