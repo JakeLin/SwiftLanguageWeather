d370f6a29d96e02cffaca66fd30b1a5eadc07127
Paul Chavarria <pjchavarria@gmail.com>
2015-12-01 20:30:42 -0800

    Removed unneeded syntax and methods

--- SwiftWeather/WeatherViewController.swift
+++ SwiftWeather/WeatherViewController.swift
@@ -20,11 +20,6 @@ class WeatherViewController: UIViewController {
     viewModel = WeatherViewModel()
     viewModel?.startLocationService()
   }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
   // MARK: ViewModel
   var viewModel: WeatherViewModel? {
