9cc34bf632a1b0822e41d9cbaad2e2e925a91352
Saul Urias <surias@nearsoft.com>
2018-06-11 14:18:27 -0700

    Refactor WeatherViewController

--- SwiftWeather/WeatherViewController.swift
+++ SwiftWeather/WeatherViewController.swift
@@ -9,113 +9,116 @@ import MobileCoreServices
 
 //MARK: - UIViewController Properties
 class WeatherViewController: UIViewController {
-
-  //MARK: - IBOutlets
-  @IBOutlet weak var locationLabel: UILabel!
-  @IBOutlet weak var iconLabel: UILabel!
-  @IBOutlet weak var temperatureLabel: UILabel!
-  @IBOutlet var forecastViews: [ForecastView]!
-
-  let identifier = "WeatherIdentifier"
-  
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var iconLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet var forecastViews: [ForecastView]!
+    
+    let identifier = "WeatherIdentifier"
+    
     //MARK: - Super Methods
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    viewModel = WeatherViewModel()
-    viewModel?.startLocationService()
-    setA11yIdentifiers()
-  }
-
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = WeatherViewModel()
+        viewModel?.startLocationService()
+        setA11yIdentifiers()
+    }
     
-    locationLabel.center.x  -= view.bounds.width
-    iconLabel.center.x -= view.bounds.width
-    temperatureLabel.center.x -= view.bounds.width
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureLabels()
+    }
     
-    iconLabel.alpha = 0.0
-    locationLabel.alpha = 0.0
-    temperatureLabel.alpha = 0.0
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configureLabelsWithAnimation()
+    }
     
-    UIView.animate(withDuration: 0.5, animations: {
-     self.locationLabel.center.x += self.view.bounds.width
-    })
-    
-    UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
-     self.iconLabel.center.x += self.view.bounds.width
-    }, completion: nil)
-    
-    UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
-     self.temperatureLabel.center.x += self.view.bounds.width
-    }, completion: nil)
-    
-    
-    UIView.animate(withDuration: 0.5, delay: 0.3, options: [], animations: {
-     self.iconLabel.alpha = 1.0
-    }, completion: nil)
-    
-    UIView.animate(withDuration: 0.5, delay: 0.4, options: [], animations: {
-     self.locationLabel.alpha = 1.0
-    }, completion: nil)
-    
-    UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: {
-     self.temperatureLabel.alpha = 1.0
-    }, completion: nil)
-    
-  }
-    
-
-  // MARK: ViewModel
-  var viewModel: WeatherViewModel? {
-    didSet {
-     viewModel?.location.observe {
-        [unowned self] in
-        self.locationLabel.text = $0
+    //MARK: Functions
+    func configureLabels(){
+        locationLabel.center.x  -= view.bounds.width
+        iconLabel.center.x -= view.bounds.width
+        temperatureLabel.center.x -= view.bounds.width
         
-        let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
-        attributeSet.title = self.locationLabel.text
+        iconLabel.alpha = 0.0
+        locationLabel.alpha = 0.0
+        temperatureLabel.alpha = 0.0
+    }
+    
+    func configureLabelsWithAnimation(){
+        UIView.animate(withDuration: 0.5, animations: {
+            self.locationLabel.center.x += self.view.bounds.width
+        })
         
-        let item = CSSearchableItem(uniqueIdentifier: self.identifier, domainIdentifier: "com.rushjet.SwiftWeather", attributeSet: attributeSet)
-        CSSearchableIndex.default().indexSearchableItems([item]){error in
-            if let error =  error {
-                print("Indexing error: \(error.localizedDescription)")
-            } else {
-                print("Location item successfully indexed")
+        UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+            self.iconLabel.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+            self.temperatureLabel.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: [], animations: {
+            self.iconLabel.alpha = 1.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.4, options: [], animations: {
+            self.locationLabel.alpha = 1.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: {
+            self.temperatureLabel.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    //MARK: ViewModel
+    var viewModel: WeatherViewModel? {
+        didSet {
+            viewModel?.location.observe {
+                [unowned self] in
+                self.locationLabel.text = $0
+                
+                let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
+                attributeSet.title = self.locationLabel.text
+                
+                let item = CSSearchableItem(uniqueIdentifier: self.identifier, domainIdentifier: "com.rushjet.SwiftWeather", attributeSet: attributeSet)
+                CSSearchableIndex.default().indexSearchableItems([item]){error in
+                    if let error =  error {
+                        print("Indexing error: \(error.localizedDescription)")
+                    } else {
+                        print("Location item successfully indexed")
+                    }
+                }
+            }
+            
+            viewModel?.iconText.observe {
+                [unowned self] in
+                self.iconLabel.text = $0
+            }
+            
+            viewModel?.temperature.observe {
+                [unowned self] in
+                self.temperatureLabel.text = $0
+            }
+            
+            viewModel?.forecasts.observe {
+                [unowned self] (forecastViewModels) in
+                if forecastViewModels.count >= 4 {
+                    for (index, forecastView) in self.forecastViews.enumerated() {
+                        forecastView.loadViewModel(forecastViewModels[index])
+                    }
+                }
             }
         }
     }
     
-     viewModel?.iconText.observe {
-        [unowned self] in
-        self.iconLabel.text = $0
-    }
-    
-     viewModel?.temperature.observe {
-        [unowned self] in
-        self.temperatureLabel.text = $0
-    }
-    
-     viewModel?.forecasts.observe {
-        [unowned self] (forecastViewModels) in
-        if forecastViewModels.count >= 4 {
-            for (index, forecastView) in self.forecastViews.enumerated() {
-                forecastView.loadViewModel(forecastViewModels[index])
-            }
-            }
-        }
-      }
-    }
-
     //MARK: Accessibility
     func setA11yIdentifiers() {
         locationLabel.accessibilityIdentifier = "a11y_current_city"
         iconLabel.accessibilityIdentifier = "a11y_wheather_icon"
         temperatureLabel.accessibilityIdentifier = "a11y_wheather_temperature"
     }
-
-
 }
