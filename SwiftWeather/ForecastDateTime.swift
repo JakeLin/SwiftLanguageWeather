21a762ebc5beabbf359a221fdde0b8ff42a38772
Saul Urias <surias@nearsoft.com>
2018-06-11 17:00:50 -0700

    Fixed ForecastDateTime Tests Adding TimeZone

--- SwiftWeather/ForecastDateTime.swift
+++ SwiftWeather/ForecastDateTime.swift
@@ -6,16 +6,19 @@
 import Foundation
 
 struct ForecastDateTime {
-  let rawDate: Double
-
-  init(_ date: Double) {
-    rawDate = date
-  }
-
-  var shortTime: String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "HH:mm"
-    let date = Date(timeIntervalSince1970: rawDate)
-    return dateFormatter.string(from: date)
-  }
+    let rawDate: Double
+    let timeZone: TimeZone
+    
+    init(date: Double, timeZone: TimeZone) {
+        self.timeZone = timeZone
+        self.rawDate = date
+    }
+    
+    var shortTime: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = timeZone
+        dateFormatter.dateFormat = "HH:mm"
+        let date = Date(timeIntervalSince1970: rawDate)
+        return dateFormatter.string(from: date)
+    }
 }
