acd70a1e0d20448f3460e87c62f603e1b6b10007
Jake Lin <jakelinau@gmail.com>
2018-10-20 15:54:50 +1100

    Create a new project

---
+++ App/SwiftWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftWeather
+//
+//  Created by Jake Lin on 20/10/18.
+//  Copyright © 2018 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
